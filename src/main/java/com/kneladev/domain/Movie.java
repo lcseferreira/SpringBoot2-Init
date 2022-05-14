2ea06886d9fceb6b91768ee25f5158469fc00495
Lucas <lcseferreira@hotmail.com>
2022-05-14 20:51:52 -0300

    Iniciando uma aplicação Spring Boot

---
+++ src/main/java/com/kneladev/domain/Movie.java
@@ -0,0 +1,13 @@
+package com.kneladev.domain;
+
+public class Movie {
+
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public Movie() {
+    }
+}
---
+++ src/main/java/com/kneladev/start/ApplicationStart.java
@@ -0,0 +1,11 @@
+package com.kneladev.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
