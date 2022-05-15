ca35b2223643d49426094eec04228c24f7df6e15
Lucas <lcseferreira@hotmail.com>
2022-05-14 21:27:30 -0300

    DevTools e Hot Swap

--- src/main/java/com/kneladev/start/ApplicationStart.java
+++ src/main/java/com/kneladev/start/ApplicationStart.java
@@ -2,12 +2,14 @@ package com.kneladev.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.kneladev")
 @Configuration
+@SpringBootApplication // Tem todas anotações que precisamos
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
